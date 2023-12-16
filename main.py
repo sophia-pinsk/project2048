ca7f520c57f35cf1afeaa1bdfccd9da85627e703
Vlellw <132191467+Vlellw@users.noreply.github.com>
2023-12-16 17:47:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
-print(123)
+import pygame
 
-print("Софа всегда права")
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(0, 0, 0), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                    self.cell_size), 1)
+
+
+pygame.init()
+size = 1200, 600
+screen = pygame.display.set_mode((1200, 700))
+pygame.display.set_caption('Инициализация игры')
+board = Board(4, 4)
+board.set_view(450, 150, 70)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((250, 250, 250))
+    board.render(screen)
+    pygame.display.flip()
+pygame.quit()
